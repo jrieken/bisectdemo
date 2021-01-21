c3a83ef9c25f61c7e6ddfeac7d35cb95ca4510bb
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-21 11:25:56 +0100

    be annoying right from the start

--- extension.js
+++ extension.js
@@ -2,13 +2,22 @@ const vscode = require('vscode');
 
 module.exports.activate = function () {
 
-    vscode.window.onDidChangeTextEditorSelection(event => {
-        // annoying: whenever you cursor is on the word 'bisect' it is being
-        // reset to document start...
-        const { textEditor, selections } = event;
-        const wordRange = textEditor.document.getWordRangeAtPosition(selections[0].active)
+    function maybeResetCursor(textEditor) {
+        // annoying: whenever your cursor is on the word 'bisect' it is 
+        // being reset to the document start...
+        const wordRange = textEditor.document.getWordRangeAtPosition(textEditor.selection.active)
         if (textEditor.document.getText(wordRange).toLowerCase() === 'bisect') {
             textEditor.selection = new vscode.Selection(0, 0, 0, 0);
         }
+    }
+
+    // whenever the selection changes there is a chance to annoy someone
+    vscode.window.onDidChangeTextEditorSelection(event => {
+        maybeResetCursor(event.textEditor);
     });
+
+    // right after start/activate take the chance to annoy someone
+    if (vscode.window.activeTextEditor) {
+        maybeResetCursor(vscode.window.activeTextEditor);
+    }
 }
